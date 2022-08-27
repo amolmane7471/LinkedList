b1bfa876c76169fde6ef3a1c04763948100a4d93
amolmane7471 <maneamol3233@gmail.com>
2022-08-27 22:09:57 +0530

    [Amol]: added linked list prgram to display msg

---
+++ src/com/bridgelabz/LinkedList/LinkedList.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.LinkedList;
+/*
+ * purpose : we have create simple linked list to manipulate operations on 3 values
+ * @author Amol
+ * @since 2022/08/27
+ */
+public class LinkedList {
+
+   public static void main(String[] args) {
+	   System.out.println("<<<< Welcome To LinkedList >>>>");
+   }
+}
---
+++ src/com/bridgelabz/LinkedList/Node.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.LinkedList;
+
+public class Node {
+	int key;
+	Node next;
+	
+	public void setKey(int key) {
+		this.key = key;
+	}
+	
+	public int getKey() {
+		return key;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+}
