a5c06f65b7747d866390e7c1f79643a312f04cdd
amolmane7471 <maneamol3233@gmail.com>
2022-08-28 09:25:23 +0530

    [Amol]: added linked list UC1 for create simple linkedlist

--- src/com/bridgelabz/LinkedList/LinkedList.java
+++ src/com/bridgelabz/LinkedList/LinkedList.java
@@ -5,8 +5,33 @@ package com.bridgelabz.LinkedList;
  * @since 2022/08/27
  */
 public class LinkedList {
-
-   public static void main(String[] args) {
+	/*
+	 * UC1 : create a simple linked list of 56,30,70
+	 */
+	public static void main(String[] args) {
 	   System.out.println("<<<< Welcome To LinkedList >>>>");
+	   /*
+	    * create a object of node 
+	    * head pointer point to first node and 
+	    * assign second node  to first node next,
+	    * third node to the second node next.
+	    */
+	   Node firstNode = new Node(56);
+	   Node secondNode = new Node(30);
+	   Node thirdNode = new Node(70);
+	   Node head = firstNode;
+	   firstNode.next = secondNode;
+	   secondNode.next = thirdNode;
+	   /*
+	    * assign head to temp variable
+	    * and check temp is null or not,if not null print first element of list
+	    */
+	   Node temp = head;
+	   while(temp != null)
+	   {
+		   System.out.print(temp.key+ "->");
+		   temp = temp.next;
+	   }
+	}
    }
-}
+
